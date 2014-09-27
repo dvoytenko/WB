1987a7f4717cd1ee76f00c372f1ec60754d6a714
Dima Voytenko <dvoytenko@gmail.com>
2014-09-27 01:15:02 -0700

    wip

--- wb-test/src/main/java/wb/model/AAA.java
+++ wb-test/src/main/java/wb/model/AAA.java
@@ -10,7 +10,7 @@ public class AAA {
 	
 	public static void main(String[] args) throws Exception {
 		
-		final String scr = "script7";
+		final String scr = "script6";
 		
 		System.out.println("Script: " + scr);
 		
